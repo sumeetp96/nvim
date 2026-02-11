4708586b14c64f8bd897c1813f2b72835391c4d6
Sumeet Prajapati <sumeetp@york.ie>
2026-02-11 19:30:55 +0530

    Refactoring

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,67 +1,67 @@
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.hl.on_yank()
-  end,
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- Auto format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
 -- LSP - Client:on_attach
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-    if client:supports_method("textDocument/implementation") then
-      local map = function(keys, func, desc)
-        vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
-      end
+		if client:supports_method("textDocument/implementation") then
+			local map = function(keys, func, desc)
+				vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
+			end
 
-      -- Common bindings
-      map("gd", vim.lsp.buf.definition, "Go to definition")
-      map("gD", vim.lsp.buf.declaration, "Go to declaration")
-      map("gr", vim.lsp.buf.references, "Show references")
-      map("gi", vim.lsp.buf.implementation, "Go to implementation")
-      map("K", vim.lsp.buf.hover, "Hover documentation")
-      map("<leader>sh", vim.lsp.buf.signature_help, "Signature help")
-      map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-      map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-      map("<leader>cf", function()
-        vim.lsp.buf.format({ async = true })
-      end, "Format")
+			-- Common bindings
+			map("gd", vim.lsp.buf.definition, "Go to definition")
+			map("gD", vim.lsp.buf.declaration, "Go to declaration")
+			map("gr", vim.lsp.buf.references, "Show references")
+			map("gi", vim.lsp.buf.implementation, "Go to implementation")
+			map("K", vim.lsp.buf.hover, "Hover documentation")
+			map("<leader>sh", vim.lsp.buf.signature_help, "Signature help")
+			map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+			map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+			map("<leader>cf", function()
+				vim.lsp.buf.format({ async = true })
+			end, "Format")
 
-      -- Diagnostic keymaps
-      map("[d", function()
-        vim.diagnostic.jump({ count = 1, float = true })
-      end, "Previous diagnostic")
-      map("]d", function()
-        vim.diagnostic.jump({ count = -1, float = true })
-      end, "Next diagnostic")
-      map("<leader>d", vim.diagnostic.open_float, "Show diagnostic")
-      map("<leader>q", vim.diagnostic.setloclist, "Diagnostic list")
+			-- Diagnostic keymaps
+			map("[d", function()
+				vim.diagnostic.jump({ count = 1, float = true })
+			end, "Previous diagnostic")
+			map("]d", function()
+				vim.diagnostic.jump({ count = -1, float = true })
+			end, "Next diagnostic")
+			map("<leader>d", vim.diagnostic.open_float, "Show diagnostic")
+			map("<leader>q", vim.diagnostic.setloclist, "Diagnostic list")
 
-      -- Specific to JS/TS
-      if client.name == "ts_ls" then
-        local function ts_action(kind)
-          vim.lsp.buf.code_action({
-            apply = true,
-            context = { only = { kind }, diagnostics = {} },
-          })
-        end
-        map("<leader>cA", "<Cmd>LspTypescriptSourceAction<CR>", "Typescript Source Action")
-        map("<leader>co", function()
-          ts_action("source.organizeImports.ts")
-        end, "Organize imports")
-        map("<leader>cm", function()
-          ts_action("source.addMissingImports.ts")
-        end, "Add missing imports")
-      end
-    end
-  end,
+			-- Specific to JS/TS
+			if client.name == "ts_ls" then
+				local function ts_action(kind)
+					vim.lsp.buf.code_action({
+						apply = true,
+						context = { only = { kind }, diagnostics = {} },
+					})
+				end
+				map("<leader>cA", "<Cmd>LspTypescriptSourceAction<CR>", "Typescript Source Action")
+				map("<leader>co", function()
+					ts_action("source.organizeImports.ts")
+				end, "Organize imports")
+				map("<leader>cm", function()
+					ts_action("source.addMissingImports.ts")
+				end, "Add missing imports")
+			end
+		end
+	end,
 })
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,70 +1,70 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      on_attach = function(bufnr)
-        local gitsigns = require("gitsigns")
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
 
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-        end
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+				end
 
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then
-            vim.cmd.normal({ "]c", bang = true })
-          else
-            gitsigns.nav_hunk("next")
-          end
-        end, "Next Hunk")
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end, "Next Hunk")
 
-        map("n", "[c", function()
-          if vim.wo.diff then
-            vim.cmd.normal({ "[c", bang = true })
-          else
-            gitsigns.nav_hunk("prev")
-          end
-        end, "Previous Hunk")
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end, "Previous Hunk")
 
-        -- Actions
-        map("n", "<leader>hs", gitsigns.stage_hunk, "Stage Hunk")
-        map("n", "<leader>hr", gitsigns.reset_hunk, "Reset Hunk")
+				-- Actions
+				map("n", "<leader>hs", gitsigns.stage_hunk, "Stage Hunk")
+				map("n", "<leader>hr", gitsigns.reset_hunk, "Reset Hunk")
 
-        map("v", "<leader>hs", function()
-          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, "Stage Hunk")
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Stage Hunk")
 
-        map("v", "<leader>hr", function()
-          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, "Reset Hunk")
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Reset Hunk")
 
-        map("n", "<leader>hS", gitsigns.stage_buffer, "Stage Buffer")
-        map("n", "<leader>hR", gitsigns.reset_buffer, "Reset Buffer")
-        map("n", "<leader>hp", gitsigns.preview_hunk, "Preview Hunk")
-        map("n", "<leader>hi", gitsigns.preview_hunk_inline, "Preview Hunk Inline")
+				map("n", "<leader>hS", gitsigns.stage_buffer, "Stage Buffer")
+				map("n", "<leader>hR", gitsigns.reset_buffer, "Reset Buffer")
+				map("n", "<leader>hp", gitsigns.preview_hunk, "Preview Hunk")
+				map("n", "<leader>hi", gitsigns.preview_hunk_inline, "Preview Hunk Inline")
 
-        map("n", "<leader>hb", function()
-          gitsigns.blame_line({ full = true })
-        end, "Blame Line")
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end, "Blame Line")
 
-        map("n", "<leader>hd", gitsigns.diffthis, "Diff Hunk")
+				map("n", "<leader>hd", gitsigns.diffthis, "Diff Hunk")
 
-        map("n", "<leader>hD", function()
-          gitsigns.diffthis("~")
-        end, "Diff Buffer")
+				map("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end, "Diff Buffer")
 
-        map("n", "<leader>hQ", function()
-          gitsigns.setqflist("all")
-        end, "Quick Fix list all")
-        map("n", "<leader>hq", gitsigns.setqflist, "Quick Fix list")
+				map("n", "<leader>hQ", function()
+					gitsigns.setqflist("all")
+				end, "Quick Fix list all")
+				map("n", "<leader>hq", gitsigns.setqflist, "Quick Fix list")
 
-        -- Toggles
-        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, "Toogle Blame Line")
-        map("n", "<leader>tw", gitsigns.toggle_word_diff, "Toggle Word Diff")
+				-- Toggles
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame, "Toogle Blame Line")
+				map("n", "<leader>tw", gitsigns.toggle_word_diff, "Toggle Word Diff")
 
-        -- Text object
-        map({ "o", "x" }, "ih", gitsigns.select_hunk, "Select Hunk")
-      end,
-    },
-  },
+				-- Text object
+				map({ "o", "x" }, "ih", gitsigns.select_hunk, "Select Hunk")
+			end,
+		},
+	},
 }
